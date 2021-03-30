7679b4d6f912f9de228afc19e96dd8e8ed347030
Tony Rowan <trowan812@gmail.com>
2021-03-30 19:00:21 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Rails Application Template
+My personal Rails Application Template. Geared towards hobby projects
+running on Heroku and my own preferences.
+
+## Usage
+To create a new Rails app with this template (and the options it is
+best suited for) run:
+
+```
+rails new hello-heroku --database=postgresql --webpack=stimulus --minimal --skip-sprockets --skip-test --template=https://raw.githubusercontent.com/tony-rowan/rails-appliction-template/main/template.rb
+```
+
+## Exaplanation
+This template does a few things.
+
+- Completely rewrites the Gemfile to make it easier on the eyes, but drops the Windows support
+- Installs RSpec with sensible defaults
+- Installs Rubocop with a sensible styles guide
+- Installs factory_bot and sets the generator to have the postfix `_factory`
+- Configures the project to use asdf - a `.tool-versions` file rather than a `.ruby-version` file
+- Installs and configures Prettier
+- Installs and configures husky and lint-staged to run Prettier and Rubocop on commit
+- Installs Tailwindcss
+- Removes sprockets and all non-webpack based assets entirely
+- Adds a `docker-compose.yml` file so you don't have to run Postgres locally
---
+++ template.rb
@@ -0,0 +1,161 @@
+remove_file 'Gemfile'
+
+file 'Gemfile', <<~RUBY
+  ruby '#{RUBY_VERSION}'
+
+  source 'https://rubygems.org'
+  git_source(:github) { |repo| "https://github.com/\#{repo}.git" }
+
+  gem 'rails'
+
+  gem 'bcrypt'
+  gem 'bootsnap', require: false
+  gem 'pg'
+  gem 'puma'
+  gem 'turbolinks'
+  gem 'webpacker'
+
+  group :development, :test do
+    gem 'byebug'
+    gem 'factory_bot_rails'
+    gem 'rspec-rails'
+  end
+
+  group :test do
+    gem 'capybara'
+    gem 'capybara-screenshot'
+    gem 'launchy'
+    gem 'selenium-webdriver'
+    gem 'shoulda-matchers'
+    gem 'webdrivers'
+  end
+
+  group :development do
+    gem 'listen'
+    gem 'rubocop'
+    gem 'rubocop-rails'
+    gem 'rubocop-rspec'
+    gem 'spring'
+    gem 'web-console'
+  end
+RUBY
+
+initializer 'generators.rb', <<~RUBY
+  Rails.application.config.generators do |g|
+    g.test_framework :rspec,
+      fixtures: false,
+      view_specs: false,
+      helper_specs: false,
+      routing_specs: false,
+      request_specs: false,
+      controller_specs: false
+    g.stylesheets :false
+    g.helper :false
+    g.factory_bot suffix: 'factory'
+  end
+RUBY
+
+inject_into_file 'bin/setup', after: "puts \"\\n== Preparing database ==\"\n" do
+  "  system! 'docker-compose up -d'\n"
+end
+file 'docker-compose.yml', <<-YML
+  version: "3.9"
+
+  services:
+    db:
+      image: postgres
+      volumes:
+        - ./tmp/db:/var/lib/postgresql/data
+      environment:
+        POSTGRES_PASSWORD: password
+      ports:
+        - "5432:5432"
+YML
+inject_into_file 'config/database.yml', after: "adapter: postgresql\n" do
+  <<-YML
+  host: 0.0.0.0
+  username: postgres
+  password: password
+  YML
+end
+file '.rubocop.yml', <<~YML
+  inherit_from:
+    - https://raw.githubusercontent.com/tony-rowan/.rubocop.yml/main/.rubocop.yml
+YML
+
+remove_file '.ruby-version'
+file '.tool-versions', "ruby #{RUBY_VERSION}"
+
+remove_dir 'app/assets'
+add_file 'app/javascript/images/.keep'
+
+run 'yarn add --dev --exact prettier'
+run 'yarn add --dev lint-staged husky'
+
+file '.prettierrc.json', <<~JS
+  {}
+JS
+
+file '.prettierignore', <<~IGNORE
+  node_modules
+  public/packs
+  public/packs-test
+  tmp
+IGNORE
+
+file '.huskyrc.json', <<~JSON
+  {
+    "hooks": {
+      "pre-commit": ["bin/yarn lint-staged && bin/rspec"]
+    }
+  }
+JSON
+
+file '.lintstagedrc', <<~JSON
+  {
+    "**/*": [
+      "bin/yarn prettier --write --ignore-unknown",
+      "bin/bundle exec rubocop --auto-correct --only-recognized-file-types --force-exclusion"
+    ]
+  }
+JSON
+
+run 'yarn add tailwindcss@npm:@tailwindcss/postcss7-compat @tailwindcss/postcss7-compat postcss@^7 autoprefixer@^9'
+file 'tailwind.config.js', <<~JS
+  module.exports = {
+    purge: ["./app/helpers/**/*", "./app/views/**/*"],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {
+        borderColor: ["hover"],
+      },
+    },
+    plugins: [],
+  };
+JS
+file 'app/javascript/packs/application.css', <<-CSS
+  @import "tailwindcss/base";
+  @import "tailwindcss/components";
+  @import "tailwindcss/utilities";
+CSS
+prepend_file 'app/javascript/packs/application.js', 'import "./application.css";'
+
+after_bundle do
+  generate 'rspec:install'
+  remove_file '.rspec'
+  file '.rspec', <<~RSPEC
+    --format documentation
+    --order random
+    --require spec_helper
+  RSPEC
+  run 'bundle binstubs rspec-core'
+
+  git :init
+  git add: '.'
+  git commit: "-m 'Initial commit'"
+
+  run 'bin/setup'
+end
